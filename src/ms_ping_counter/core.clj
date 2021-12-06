c95e58c85aa06e838d178799fee435845a6b76a4
Pankaj Godbole <pankajsg@gmail.com>
2021-12-06 22:01:00 +0530

    For Heroku: fetch the Redis URL from the config var REDIS_URL

--- src/ms_ping_counter/core.clj
+++ src/ms_ping_counter/core.clj
@@ -9,8 +9,59 @@
   (:gen-class
     :main true)
   (:require [com.stuartsierra.component      :as component]
+            [environ.core                    :as environ]
             [ms-ping-counter.redis-component :as redis]
             [ms-ping-counter.http-server     :as http-server]))
+
+(declare get-redis-url
+         redis-url
+         get-http-port
+         http-host
+         ;;http-port
+         ^:dynamic *system*
+         main-system
+         start
+         stop)
+
+(defn -main
+  "The application's entry point"
+  []
+  (prn "core/-main:\nHello, World!")
+  (let [main-system (start (main-system))]
+    ;; Dynamically rebind *system* to the newly created SystemMap instance
+    (alter-var-root #'*system* (constantly main-system))
+    ;; Create a hook that stops the component 'system' in a controlled manner
+    ;; before the JVM completely shuts down
+    (.addShutdownHook (Runtime/getRuntime)
+                      (Thread. (partial stop main-system)))))
+
+(defn main-system
+  "Implements Component's Lifecycle protocol and creates a map of component dependencies"
+  []
+  (let [redis-url (get-redis-url)
+        http-port (get-http-port)]
+    (prn "core/main-system:\nredis-url, http-host, http-port:\n"
+         redis-url http-host http-port)
+    (component/system-map
+      :redis (redis/create-redis-instance redis-url)
+      :http-server (component/using (http-server/create-new-server http-host
+                                                                   http-port)
+                                    [:redis]))))
+
+(def redis-url #'get-redis-url)
+;;(def http-host "localhost")
+(def http-host "0.0.0.0")
+(def http-port #'get-http-port)
+
+(defn get-redis-url
+  "For Heroku: fetch the config var REDIS_URL if present, or to run locally: return
+   the default Redis URL otherwise
+   Ref: https://devcenter.heroku.com/articles/getting-started-with-clojure#define-config-vars"
+  []
+  (let [redis-url (System/getenv "REDIS_URL")]
+    (if-not (empty? redis-url)
+      redis-url
+      "redis://127.0.0.1:6379")))
 
 (defn get-http-port
   "Heroku dynamically assigns your app a port, so you can't set the port to a
@@ -21,22 +72,6 @@
     (if-not (empty? port)
       (java.lang.Long/parseLong port)
       8080)))
-
-;;(def http-host "localhost")
-(def http-host "0.0.0.0")
-(def http-port (get-http-port))
-
-(defonce ^:dynamic *system* nil)
-
-(defn main-system
-  "Implements Component's Lifecycle protocol and creates a map of component dependencies"
-  []
-  (prn "core/main-system:\nhttp-host, http-port:" http-host http-port)
-  (component/system-map
-    :redis       (redis/create-redis-instance "redis://localhost:6379")
-    :http-server (component/using (http-server/create-new-server http-host
-                                                                 http-port)
-                                  [:redis])))
 
 (defn start
   "Starts components of system in dependency order. Runs the SystemMap implementation for the
@@ -58,14 +93,4 @@
   ;; Dynamically rebind *system* back to nil
   (alter-var-root #'*system* (constantly nil)))
 
-(defn -main
-  "The application's entry point"
-  []
-  (prn "core/-main:\nHello, World!")
-  (let [main-system (start (main-system))]
-    ;; Dynamically rebind *system* to the newly created SystemMap instance
-    (alter-var-root #'*system* (constantly main-system))
-    ;; Create a hook that stops the component 'system' in a controlled manner
-    ;; before the JVM completely shuts down
-    (.addShutdownHook (Runtime/getRuntime)
-                      (Thread. (partial stop main-system)))))
+(defonce ^:dynamic *system* nil)
